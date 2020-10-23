a733cc0d50b0ec03c5884c96a7c7755870c9c024
enzoperce35 <enzoperce35@gmail.com>
2020-10-24 07:34:56 +0800

    added a driver script

--- main.rb
+++ main.rb
@@ -1,28 +1,28 @@
 require_relative 'tree.rb'
 
-bst = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
-bst2 = Tree.new([7, 11, 12, 25, 98, 423, 3085, 76, 3, 59, 2022, 2017])
+rand_num = Array.new(15) { rand(1..100) }
+bal_bst = Tree.new(rand_num)
 
-#bst.pretty_print
+puts 'A balanced binary search tree of random numbers from 1-100'
+bal_bst.pretty_print
 
-#bst.tree.right.data = nil
+puts "Balanced?: #{ bal_bst.balanced? }",
+"level-order: #{ bal_bst.level_order }",
+"preOrder: #{ bal_bst.preOrder }",
+"inOrder: #{ bal_bst.inOrder }",
+"postOrder: #{ bal_bst.postOrder }\n\n"
 
-#bst.delete(8)
-#bst.insert(25)
+bal_bst.insert(150)
+bal_bst.insert(250)
+puts 'The binary search tree added with two numbers greater than 100'
+bal_bst.pretty_print
+puts "Balanced?: #{ bal_bst.balanced? }\n\n"
 
-#p bst > bst2
-
-#p bst.preOrder
-#p bst.inOrder
-#p bst.postOrder
-#p bst.level_order
-
-#p bst.height(67)
-#p bst.depth(324)
-
-#p bst.balanced?
-#bst.rebalance
-
-#bst.pretty_print
-
-
+bal_bst.rebalance
+puts 'The rebalanced binary search tree'
+bal_bst.pretty_print
+puts "Balanced?: #{ bal_bst.balanced? }",
+"level-order: #{ bal_bst.level_order }",
+"preOrder: #{ bal_bst.preOrder }",
+"inOrder: #{ bal_bst.inOrder }",
+"postOrder: #{ bal_bst.postOrder }"
